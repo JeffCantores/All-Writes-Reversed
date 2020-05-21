c9cde71e73273f0e83f19762100ac0ff6aa65c79
IamJayce <johncarlo.jumawan212@gmail.com>
2020-05-21 13:34:10 +0800

    Initial Fix

--- view-comp/footer.php
+++ view-comp/footer.php
@@ -15,6 +15,6 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
     integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
     crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="../css/home_page_css.css">
+    <link rel="stylesheet" type="text/css" href="css/home_page_css.css">
   </body>
 </html>

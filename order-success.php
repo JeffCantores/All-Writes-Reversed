2e2dafbef161f39dc7675ad84791e53b14ab04d9
JeffCantores <jeffersoncantores13@gmail.com>
2020-05-24 17:59:02 +0800

    Added order-success page

---
+++ order-success.php
@@ -0,0 +1,25 @@
+<?php
+      require_once('view-comp/header.php');
+?>
+
+
+<br>
+<div class="container jeff">
+  <div class="view-cart-items">
+    <div class="title">
+        <strong><i>ORDER SUCCESSFUL!</i></strong>
+    </div>
+
+    <div class="body">
+      <div class="cart-items">
+            (ORDER SUCCESS MESSAGE GOES HERE)
+      </div>
+    </div>
+
+    <br>
+  </div>
+</div>
+<br>
+
+
+<?php require_once('view-comp/footer.php') ?>

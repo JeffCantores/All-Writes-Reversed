03b9ec24fbcd44d1b2a4f981be9976687f9a54ff
krishakandt <krishabecerral@gmail.com>
2020-06-09 19:35:00 +0800

    add to cart update

--- services-comp/process-add-to-cart.php
+++ services-comp/process-add-to-cart.php
@@ -16,7 +16,7 @@
             $userID = $resultUserID->fetch_assoc();
 
             if($userID){
-<<<<<<< HEAD
+
               $selectProdID;
               $cart_item = 'SELECT * from cart WHERE product_id = '.$prodID['prodID'];
               $count = $cart_item->num_rows();
@@ -27,13 +27,55 @@
                 $stmt = $db->prepare($insertToCart);
                 $stmt->bind_param('iiiii', $qty, $price, $prodID['prodID'], $userID['userID'], 0);
                 $stmt->execute();
-=======
-              $false = 0;
-              $insertToCart = 'INSERT INTO cart (qty, price, product_id, user_id, checkedOut) VALUES (?,?,?,?,?)';
-              $stmt = $db->prepare($insertToCart);
-              $stmt->bind_param('iiiii', $qty, $price, $prodID['prodID'], $userID['userID'], $false);
-              $stmt->execute();
->>>>>>> b3595cb3cb31232eaa22fb75645db32ae648b80e
+
+                if(!$stmt){
+                  throw new Exception("EXCEPTION INSERT");
+                } else {
+                  $_SESSION['addtocart'] = 'TRUE';
+                }
+              }
+            } else {
+              throw new Exception("EXCEPTION USERID");
+            }
+          } else {
+            throw new Exception("EXCEPTION PRODID");
+          }
+        }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+
+  }
+?>
+
+<?php
+  function addToCart($qty,$price, $productName){
+    try {
+      @ $db = new mysqli('127.0.0.1:3306','krimhajefcee', 'incorrect', 'awr_database');
+	   		$dbError = mysqli_connect_errno();
+		    if($dbError){
+		      	throw new Exception("DB CONNECTION ERROR");
+		    }else{
+          $selectProdID = 'SELECT products.id as prodID FROM products WHERE name = "'.$productName.'"';
+          $resultProdID = $db->query($selectProdID);
+          $prodID = $resultProdID->fetch_assoc();
+
+          if($prodID){
+            $selectUserID = 'SELECT users.id as userID FROM users WHERE username = "'.$_SESSION['username'].'"';
+            $resultUserID = $db->query($selectUserID);
+            $userID = $resultUserID->fetch_assoc();
+
+          if($userID){
+              $selectProdID;
+              $cart_item = 'SELECT * from cart WHERE product_id = '.$prodID['prodID'];
+              $count = $cart_item->num_rows();
+              if ($count>0) {
+                throw new Exception("Item already in Cart");
+              }else {
+                $insertToCart = 'INSERT INTO cart (qty, price, product_id, user_id, isCheckedOut) VALUES (?,?,?,?,?)';
+                $stmt = $db->prepare($insertToCart);
+                $stmt->bind_param('iiiii', $qty, $price, $prodID['prodID'], $userID['userID'], 0);
+                $stmt->execute();
 
                 if(!$stmt){
                   throw new Exception("EXCEPTION INSERT");

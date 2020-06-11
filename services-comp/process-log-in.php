d85e4692fc861a2cca93d57163e3d25edf041f9c
JeffCantores <jeffersoncantores13@gmail.com>
2020-06-11 16:41:28 +0800

    Added user logs (with date, user IP, and username)

--- services-comp/process-log-in.php
+++ services-comp/process-log-in.php
@@ -1,4 +1,10 @@
 <?php
+  require_once('exception/file-not-found-exception.php');
+?>
+
+<?php
+  define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
+
   function login(){
     $loginUsername = $_POST['username'];
     $loginPassword = $_POST['password'];
@@ -26,6 +32,9 @@
               $sttmnt->bind_param("i", $user['id']);
               $sttmnt->execute();
 
+              saveLogin();
+              getLogin();
+
             } else {
               $_SESSION['login-error'] = "Incorrect Password.";
             }
@@ -38,4 +47,50 @@
     }
 
   }
+
+  function saveLogin(){
+      $loginUsername = $_POST['username'];
+
+      echo "<br/>".DOCUMENT_ROOT;
+
+      $date = date('H:i, jS F Y');
+      $outputString = $date."\t"
+      .$_SERVER['REMOTE_ADDR']."\t"
+      .$loginUsername."\n";
+
+      $file = @ fopen(DOCUMENT_ROOT.'/WEBPROG-FINALS/resource/user-logs.txt', 'ab'); //writing
+
+      if (!$file) {
+        echo "<p><strong>Your order could not be processed at this time.
+            Please try again later.</strong></p>";
+      }else{
+
+          flock($file, LOCK_EX);
+          fwrite($file, $outputString, strlen($outputString));
+          flock($file, LOCK_UN);
+
+          fclose($file);
+      }
+    }
+
+    function getLogin(){
+
+      try{
+        $file = @ fopen(DOCUMENT_ROOT.'/WEBPROG-FINALS/resource/user-logs.txt', 'rb'); // reading
+
+          if (!$file) {
+            throw new FileNotFoundException('No users logged in.
+                Please try again later.', 1);
+          }else{
+            while (!feof($file)){
+              $order = fgets($file, 999);
+              echo $order.'<br>';
+            };
+
+            fclose($file);
+          }
+        }catch(FileNotFoundException $fnfe){
+          echo $fnfe->getMessage();
+        }
+      }
 ?>

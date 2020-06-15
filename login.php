3f6bffc62451e034bf66bc3e10dc23896f8b1b4a
JeffCantores <jeffersoncantores13@gmail.com>
2020-06-15 17:42:29 +0800

    Added logs for logout and remove item

--- login.php
+++ login.php
@@ -3,8 +3,10 @@
   require_once('services-comp/process-log-in.php');
   //sign up function
   if(isset($_POST['username']) && isset($_POST['password'])){
+
     //call log-in function
     login();
+
     header("Location: index.php");
   }
 
--- services-comp/remove-item.php
+++ services-comp/remove-item.php
@@ -2,7 +2,11 @@
   session_start();
   require_once('../functions/functions.php');
   if(isset($_POST['name'])){
-    removeCartItem($_POST['name']);
+
+    $prodPrice = $_POST['price'];
+    $prodQty = $_POST['qty'];
+
+    removeCartItem($_POST['name'], $prodPrice, $prodQty);
     header('Location: ../view-cart.php');
   }
 ?>
